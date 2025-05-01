7bbf255354914675842342be967fb864c7c4c097
Rhian Vinícius <rianresende497@gmail.com>
2025-05-01 17:59:59 -0300

    implement sliders logic

--- lib/pages/home_page_content.dart
+++ lib/pages/home_page_content.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ex001/stateful_widgets/app_slider.dart';
+import 'package:flutter_ex001/stateful_widgets/app_slider_label.dart';
 import 'package:flutter_ex001/theme/app_colors.dart';
 
 class HomePageContent extends StatefulWidget {
@@ -12,6 +14,28 @@ class HomePageContent extends StatefulWidget {
 
 
 class HomePageContentState extends State<HomePageContent> {
+  double _redValue = 0;
+  double _greenValue = 0;
+  double _blueValue = 0;
+
+  void _updateRedValue(double newValue) {
+    setState(() {
+      _redValue = newValue;
+    });
+  }
+
+  void _updateGreenValue(double newValue) {
+    setState(() {
+      _greenValue = newValue;
+    });
+  }
+
+  void _updateBlueValue(double newValue) {
+    setState(() {
+      _blueValue = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -42,10 +66,63 @@ class HomePageContentState extends State<HomePageContent> {
               )
             ),
             Expanded(
-              child: Column(
-                children: [
-                  
-                ],
+              child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  double totalHeight = constraints.maxHeight; 
+
+                  return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: totalHeight / 3,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppSliderLabel(label: _redValue, color: AppColors.primary),
+                              AppSlider(
+                                value: _redValue,
+                                onChanged: _updateRedValue,
+                                color: AppColors.primary
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: totalHeight / 3,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppSliderLabel(label: _greenValue, color: AppColors.primary),
+                              AppSlider(
+                                value: _greenValue,
+                                onChanged: _updateGreenValue,
+                                color: AppColors.primary
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: totalHeight / 3,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppSliderLabel(label: _blueValue, color: AppColors.primary),
+                              AppSlider(
+                                value: _blueValue,
+                                onChanged: _updateBlueValue,
+                                color: AppColors.primary
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
               )
             )
           ],
